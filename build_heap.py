6efb7a9cb14ed329d1437fcd56596590851a5083
elinazeberga <125008319+elinazeberga@users.noreply.github.com>
2023-03-19 10:18:27 +0000

    4

--- build_heap.py
+++ build_heap.py
@@ -22,10 +22,6 @@ def build_heap(data):
     for i in range(size//2, -1, -1):
         heaps(data, size, i, swaps)
         i = i+1
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
     return swaps
 
 
@@ -41,31 +37,15 @@ def main():
     elif "I" in choice:
         n = int(input())
         data = list(map(int, input().split()))
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
    
-    
-    
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
+    
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
 
     # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
